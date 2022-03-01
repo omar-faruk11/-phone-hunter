d25af4b474f1f15b22c3680d36e520c3c30b10f0
omar-faruk11 <omarabdullah1811@gmail.com>
2022-03-01 15:30:53 +0600

    add other of details

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const getSearchResult = () => {
     document.getElementById('searchError').classList.add('d-none');
     document.getElementById('phoneDetails').innerHTML = '';
     document.getElementById('addPhone').innerHTML = '';
+    document.getElementById('phoneDetailsHeading').classList.add('d-none')
+    document.getElementById('phoneHeading').classList.add('d-none');
+    document.getElementById('hrLine').classList.add('d-none')
     document.getElementById('loading').classList.remove('d-none');
     const searchValue = document.getElementById('searchFild').value;
     const search = searchValue.toLowerCase();
     if (search == '') {
-        document.getElementById('phoneDetails').innerHTML = '';
-        document.getElementById('addPhone').innerHTML = '';
-        document.getElementById('searchError').classList.remove('d-none');
         document.getElementById('loading').classList.add('d-none');
+        document.getElementById('phoneDetailsHeading').classList.add('d-none')
+        document.getElementById('phoneHeading').classList.add('d-none');
 
     } else {
         fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
@@ -20,21 +22,35 @@ const getSearchResult = () => {
 const setSearchResult = phones => {
     const phoneParentDiv = document.getElementById('addPhone');
     console.log(phones)
-    if(phones == '' ){
+    const phoneTwenty = phones.slice(0,20);
+    console.log(phoneTwenty)
+    if(phoneTwenty == '' ){
         document.getElementById('searchError').classList.remove('d-none');
         document.getElementById('loading').classList.add('d-none');
+        document.getElementById('phoneDetailsHeading').classList.add('d-none');
+        document.getElementById('phoneHeading').classList.add('d-none');
+        
     }
-    phones?.forEach(phone => {
+     
+    phoneTwenty?.forEach(phone => {
+        document.getElementById('phoneDetailsHeading').classList.add('d-none')
+        document.getElementById('phoneHeading').classList.remove('d-none');
         // console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 border-0 shadow rounded-3">
-            <img src="${phone.image}" class="card-img-top px-5 pt-3" alt="...">
+            <img src="${phone.image}" class="card-img-top my-5 mx-auto phone-size" alt="...">
             <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.brand}</p>
-                <button onclick="seeDetails('${phone.slug}')" class=" btn btn text-white btn-bg-color "> See Details</button>
+            <div class=" d-flex">
+                <div> 
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                </div>
+                <div class="ms-auto mt-auto">
+                <button onclick="seeDetails('${phone.slug}')" class=" btn btn text-white btn-bg-color"> See Details</button>
+                </div>
+            </div>
             </div>
         </div>`
         phoneParentDiv.appendChild(div);
@@ -47,31 +63,50 @@ const seeDetails = phoneId => {
     console.log(phoneId)
     fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
         .then(res => res.json())
-        .then(data => phoneDetails(data));
+        .then(data => phoneDetails(data.data));
 }
 
 const phoneDetails = details => {
-    console.log(details.data.releaseDate)
+    console.log(details.others)
+    document.getElementById('phoneDetailsHeading').classList.remove('d-none')
     const detailsParentDiv = document.getElementById('phoneDetails');
     const detailsDiv = document.createElement('div');
-    detailsDiv.classList.add('card', 'mb-3', 'w-75', 'mx-auto');
+    detailsDiv.classList.add('card', 'mb-3', 'w-100', 'mx-auto' ,'shadow', 'rounded-3');
     detailsDiv.innerHTML = `<div class="row g-0">
-     <div class="col-md-4">
-        <img src="${details.data.image} " class="img-fluid rounded-start" alt="...">
+     <div class="col-md-2">
+        <img src="${details.image} " class=" img-fluid p-3 rounded-start" alt="...">
      </div>
-     <div class="col-md-8">
-      <div class="card-body">
-      <h5 class="card-title"> ${details.data.name}</h5>
-      <h6 >Details:</h6>
-      <ul>
-          <li class="card-text">storage: ${details.data.mainFeatures.storage}</li>
-          <li class="card-text">displaySize: ${details.data.mainFeatures.displaySize}</li>
-          <li class="card-text">chipSet: ${details.data.mainFeatures.chipSet}</li>
-          <li class="card-text">memory: ${details.data.mainFeatures.memory}</li>
-      </ul>
-      <p class="primary-color"> releaseDate: ${details.data.releaseDate ? details.data.releaseDate:'No release data' }</p>
-      </div>
-    </div>
-  </div>`
+        <div class="col-md-6">
+            <div class="card-body">
+                <h4 class="card-title mb-0"> ${details.name}</h4>
+                <p class="primary-color mb-2">${details.releaseDate ? details.releaseDate:'No release day found'}</p>
+                <h6 >Details:</h6>
+                <p class="card-text mb-1"> <span class=" fw-bold">storage:</span> ${details.mainFeatures.storage}</p>
+                <p class="card-text mb-1"> <span class=" fw-bold">displaySize:</span>  ${details.mainFeatures.displaySize}</p>
+                <p class="card-text mb-1"> <span class=" fw-bold">chipSet:</span>  ${details.mainFeatures.chipSet}</p>
+                <p class="card-text mb-1"> <span class=" fw-bold">memory:</span>  ${details.mainFeatures.memory}</p>
+                
+            </div>
+        </div>
+            <div class="col-md-4">
+                <div class=" card-body">
+                    <h6 >others:</h6>
+                        <p class="card-text mb-1"> <span class=" fw-bold">WLAN:</span> ${details.others ?.WLAN ? details.others.WLAN:""}</p>
+                        <p class="card-text mb-1"> <span class=" fw-bold">Bluetooth:</span>  ${details.others ?.Bluetooth ? details.others.Bluetooth:""}</p>
+                        <p class="card-text mb-1"> <span class=" fw-bold">GPS:</span>  ${details.others ?.GPS ? details.others.GPS:""}</p>
+                        <p class="card-text mb-1"> <span class=" fw-bold">NFC:</span>  ${details.others ?.NFC ? details.others.NFC:""}</p>
+                        <p class="card-text mb-1"> <span class=" fw-bold">Radio:</span>  ${details.others ?.Radio ? details.others.Radio:""}</p>
+                        <p class="card-text mb-1"> <span class=" fw-bold">USB:</span>  ${details.others ?.USB ? details.others.USB:""}</p>
+                <div>
+            </div>
+        </div>`
     detailsParentDiv.appendChild(detailsDiv);
+    document.getElementById('hrLine').classList.remove('d-none')
 }
+
+
+
+
+// document.getElementById('phoneDetails').innerHTML = '';
+// document.getElementById('addPhone').innerHTML = '';
+// document.getElementById('searchError').classList.remove('d-none');
